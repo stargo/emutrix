b69767bb11afc5e4368f4ec03392347897ea4840
cpolymeris <cpolymeris@f1680f85-d178-6c20-23b9-708cb23fb324>
2010-08-31 02:34:36 +0000

    Quit gracefully.

--- src/main.cc
+++ src/main.cc
@@ -5,6 +5,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    a.setApplicationName(APPLICATION_NAME);
     qDebug() << "Starting " << APPLICATION_NAME << "...";
     MainWindow w;
     w.show();
