247cdcba8c3152fd0b781070584c1ab6127cb2b1
cpolymeris <cpolymeris@f1680f85-d178-6c20-23b9-708cb23fb324>
2010-09-03 09:16:25 +0000

    L/R link for output channels.

--- src/mainwindow_slots.cc
+++ src/mainwindow_slots.cc
@@ -15,8 +15,34 @@
  */
 
 #include "mainwindow.h"
+#include "ui_mainwindow.h"
 #include <QDebug>
 #include <QSlider>
+
+//// HELPER FUNCTIONS
+
+void MainWindow::checkLinked(QButtonGroup * bg, QButtonGroup * linked)
+{
+    if (!ui->link->isChecked())
+        return;
+    int ix = bg->checkedId();
+    if (ix == -2);
+    else if (linked->objectName().endsWith('l'))
+        ix++;
+    else if(linked->objectName().endsWith('r'))
+        ix--;
+    /// TODO make this work for input channels.
+    /*else if (-ix >= 5 && -ix <= 14 && ix % 2 == 0) // checked an even id => right channel
+        ix++; // change left
+    else if (-ix >= 5 && -ix <= 14 && ix % 2 == 1) // checked an odd id => left channel
+        ix--; // change right
+        */
+    else
+        return;
+    if (linked->checkedId() != ix)
+        // Using click() because it fires the appropiate signals
+        linked->button(ix)->click();
+}
 
 //// GENERAL SIGNALS
 void MainWindow::on_panic_pressed()
@@ -143,11 +169,13 @@ void MainWindow::on_b16_buttonClicked(int i)
 void MainWindow::on_b0l_buttonClicked(int i)
 {
     matrixWriteEnum("0202 DAC Left Playback Enum", i);
+    checkLinked(ui->b0l, ui->b0r);
 }
 
 void MainWindow::on_b0r_buttonClicked(int i)
 {
     matrixWriteEnum("0202 DAC Right Playback Enum", i);
+    checkLinked(ui->b0r, ui->b0l);
 }
 
 void MainWindow::on_ba0_buttonClicked(int i)
@@ -193,69 +221,83 @@ void MainWindow::on_ba7_buttonClicked(int i)
 void MainWindow::on_bsl_buttonClicked(int i)
 {
     matrixWriteEnum("1010 SPDIF Left Playback Enum", i);
+    checkLinked(ui->bsl, ui->bsr);
 }
 
 void MainWindow::on_bsr_buttonClicked(int i)
 {
     matrixWriteEnum("1010 SPDIF Right Playback Enum", i);
+    checkLinked(ui->bsr, ui->bsl);
 }
 
 void MainWindow::on_b1l_buttonClicked(int i)
 {
     matrixWriteEnum("Dock DAC1 Left Playback Enum", i);
+    checkLinked(ui->b1l, ui->b1r);
 }
 
 void MainWindow::on_b1r_buttonClicked(int i)
 {
     matrixWriteEnum("Dock DAC1 Right Playback Enum", i);
+    checkLinked(ui->b1r, ui->b1l);
 }
 
 void MainWindow::on_b2l_buttonClicked(int i)
 {
     matrixWriteEnum("Dock DAC2 Left Playback Enum", i);
+    checkLinked(ui->b2l, ui->b2r);
 }
 
 void MainWindow::on_b2r_buttonClicked(int i)
 {
     matrixWriteEnum("Dock DAC2 Right Playback Enum", i);
+    checkLinked(ui->b2r, ui->b2l);
 }
 
 void MainWindow::on_b3l_buttonClicked(int i)
 {
     matrixWriteEnum("Dock DAC3 Left Playback Enum", i);
+    checkLinked(ui->b3l, ui->b3r);
 }
 
 void MainWindow::on_b3r_buttonClicked(int i)
 {
     matrixWriteEnum("Dock DAC3 Right Playback Enum", i);
+    checkLinked(ui->b3r, ui->b3l);
 }
 
 void MainWindow::on_b4l_buttonClicked(int i)
 {
     matrixWriteEnum("Dock DAC4 Left Playback Enum", i);
+    checkLinked(ui->b4l, ui->b4r);
 }
 
 void MainWindow::on_b4r_buttonClicked(int i)
 {
     matrixWriteEnum("Dock DAC4 Right Playback Enum", i);
+    checkLinked(ui->b4r, ui->b4l);
 }
 
 void MainWindow::on_bpl_buttonClicked(int i)
 {
     matrixWriteEnum("Dock Phones Left Playback Enum", i);
+    checkLinked(ui->bpl, ui->bpr);
 }
 
 void MainWindow::on_bpr_buttonClicked(int i)
 {
     matrixWriteEnum("Dock Phones Right Playback Enum", i);
+    checkLinked(ui->bpr, ui->bpl);
 }
 
 void MainWindow::on_bdsl_buttonClicked(int i)
 {
     matrixWriteEnum("Dock SPDIF Left Playback Enum", i);
+    checkLinked(ui->bdsl, ui->bdsr);
 }
 
 void MainWindow::on_bdsr_buttonClicked(int i)
 {
     matrixWriteEnum("Dock SPDIF Right Playback Enum", i);
+    checkLinked(ui->bdsr, ui->bdsl);
 }
