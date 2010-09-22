542ad23f7a178b7513b84d64095df7d5689fa49d
cpolymeris <cpolymeris@f1680f85-d178-6c20-23b9-708cb23fb324>
2010-09-22 05:31:35 +0000

    Matrix channels visibility toggling + some not very convincing layout changes.

---
+++ trunk/src/matrix_visibility.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2010 Camilo Polymeris
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef MATRIX_VISIBILITY_H
+#define MATRIX_VISIBILITY_H
+
+const int matrixCommonCols[] = { 0, -1 };
+const int matrixCommonRows[] = { 0, 1, 2, -1 };
+const int matrixALSAcols[] = { 2, 3, 4, 5, 6, 7, -1 };
+const int matrixALSArows[] = { 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, -1 };
+const int matrix1010cols[] = { 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, -1 };
+const int matrix1010rows[] = { 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, -1 };
+const int matrix0202cols[] = { 8, 9, -1 };
+const int matrix0202rows[] = { 11, 12, -1 };
+const int matrixDockCols[] = { 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, -1 };
+const int matrixDockRows[] = { 3, 4, 5, 6, 7, 8, 9, 10, 13, 14, -1 };
+const int matrix0404cols[] = { 7, 8, 17, 18, -1 };
+const int matrix0404rows[] = { 11, 12, 13, 14, -1 };
+
+#endif // MATRIX_VISIBILITY_H
